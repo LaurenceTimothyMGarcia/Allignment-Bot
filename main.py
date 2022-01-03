e70b2f926e31d961ec7254cbb29baba35a70d96c
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-01-03 11:18:25 -0800

    got bot working
    bot works on start up now

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import discord
 import random
 
+botID = open("token.txt", "r")
+
+TOKEN = botID.read()
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+   print("Bot turned on")
+
+
+
+botID.close()
+
+client.run(TOKEN)
