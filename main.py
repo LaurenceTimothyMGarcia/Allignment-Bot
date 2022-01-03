16f5078e86291447074fa8fd428a8e90c01bc643
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-01-03 11:37:09 -0800

    bot ignores its own messages

--- main.py
+++ main.py
@@ -15,11 +15,18 @@ async def on_ready():
 #When each message plays
 @client.event
 async def on_message(message):
+   #ensures bot does not read itself
+   if message.author == client.user:
+      return
+
+   #Takes Discord account info and message content
    username = str(message.author)
    usernameID = str(message.author.id)
    user_message = str(message.content)
    channel = str(message.channel.name)
    print(f'{username} {usernameID}: {user_message} ({channel})')
+
+   
 
 botID.close()
 
